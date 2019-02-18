5d14e6c4a807166c9086820157863609044b3f50
silasferreiracarneiro <silasferreira96@hotmail.com>
2019-02-17 23:24:23 -0300

    Configurado o banco local no app, e implementado no loginrepository

--- app/src/main/java/com/example/bank/bankapp/data/repository/LoginRepository.kt
+++ app/src/main/java/com/example/bank/bankapp/data/repository/LoginRepository.kt
@@ -1,19 +1,30 @@
 package com.example.bank.bankapp.data.repository
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.os.StrictMode
 import com.example.bank.bankapp.data.LoginAPI
+import com.example.bank.bankapp.data.Sqlite.UserTable
 import com.example.bank.bankapp.infra.RetrofitClient
 import com.example.bank.bankapp.data.dto.UsuarioResponseDTO
 import com.example.bank.bankapp.data.dto.LoginDto
+import com.example.bank.bankapp.infra.DbSettings
 import retrofit2.Response
 
-class LoginRepository {
+class LoginRepository(ctx: Context) {
 
     var provider: LoginAPI
+
+    private var instance : DbSettings? = null
+    protected var db : SQLiteDatabase? = null
 
     init {
         val retrofit = RetrofitClient.instance
         provider = retrofit.create(LoginAPI::class.java)
+
+        this.instance = DbSettings.getInstance(ctx)
+        this.db = instance?.writableDatabase
     }
 
     fun sendLogin(usuario: LoginDto) : Response<UsuarioResponseDTO> {
@@ -23,5 +34,18 @@ class LoginRepository {
         var call = provider.login(usuario)
         var response = call.execute()
         return response
+    }
+
+    fun insertUser(usuario: LoginDto){
+        try {
+            val values = ContentValues()
+
+            values.put(UserTable.LOGIN, usuario.user)
+            values.put(UserTable.SENHA, usuario.password)
+
+            db?.insertOrThrow(UserTable.NAME_TABLE_USUARIO, null, values)?.toInt()!!
+        }catch (ex : Exception){
+            ex.printStackTrace()
+        }
     }
 }
--- app/src/main/java/com/example/bank/bankapp/infra/DbSettings.kt
+++ app/src/main/java/com/example/bank/bankapp/infra/DbSettings.kt
@@ -1,4 +1,34 @@
 package com.example.bank.bankapp.infra
 
-class DbSettings {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.util.Log
+import com.example.bank.bankapp.data.Sqlite.UserTable
+import org.jetbrains.anko.db.ManagedSQLiteOpenHelper
+
+class DbSettings(var ctx : Context) : ManagedSQLiteOpenHelper(ctx, "BANK_BD_APP", null , 1) {
+
+    companion object {
+        private var instance : DbSettings? = null
+
+        @Synchronized
+        fun getInstance(ctx : Context) : DbSettings{
+            if(instance == null)
+            {
+                instance = DbSettings(ctx.applicationContext)
+            }
+            return instance!!
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(UserTable.CREATE_TABLE_USUARIO)
+        Log.e("CREATE_TABLES", "TABELAS CRIADAS")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS "+UserTable.CREATE_TABLE_USUARIO)
+        Log.e("UPDATE_TABLES", "TABELAS DELETADAS")
+        onCreate(db)
+    }
 }
