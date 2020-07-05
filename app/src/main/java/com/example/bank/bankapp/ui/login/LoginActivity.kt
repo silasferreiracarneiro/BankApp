eec7b5f29e7e675f39a64de8fb57841d9044c60e
silasferreiracarneiro <silasferreira96@hotmail.com>
2020-07-05 12:50:29 -0300

    Adicionando o snackbar para mostrar o erro para o usuário

--- app/src/main/java/com/example/bank/bankapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/bank/bankapp/ui/login/LoginActivity.kt
@@ -1,16 +1,20 @@
 package com.example.bank.bankapp.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.bank.bankapp.R
 import com.example.bank.bankapp.model.UserAccount
+import com.google.android.material.snackbar.Snackbar
+
 
 class LoginActivity : AppCompatActivity(), LoginContract.View {
 
     private lateinit var presenter : LoginPresenter
 
+    private lateinit var coordinatorLayout: ConstraintLayout
     private lateinit var btnLogin: Button
     private lateinit var edtUsername: EditText
     private lateinit var edtPassword: EditText
@@ -29,6 +33,7 @@ class LoginActivity : AppCompatActivity(), LoginContract.View {
     }
 
     private fun bindProperties() {
+        this.coordinatorLayout = findViewById(R.id.coordinator_layout)
         this.btnLogin = findViewById(R.id.btn_login)
         this.edtUsername = findViewById(R.id.input_username)
         this.edtPassword = findViewById(R.id.input_password)
@@ -58,7 +63,7 @@ class LoginActivity : AppCompatActivity(), LoginContract.View {
     }
 
     override fun errorLogin(message: String?) {
-
+        Snackbar.make(coordinatorLayout, message ?: getString(R.string.error_default), Snackbar.LENGTH_LONG).show()
     }
 
     override fun sucessCallApi(user: UserAccount?) {
