7dad47f3879457381351f8d91c0dd4cb33a067ca
silasferreiracarneiro <silasferreira96@hotmail.com>
2020-07-05 17:54:04 -0300

    Feito as validações dos campos

--- app/src/main/java/com/example/bank/bankapp/ui/login/LoginUsecase.kt
+++ app/src/main/java/com/example/bank/bankapp/ui/login/LoginUsecase.kt
@@ -1,17 +1,20 @@
 package com.example.bank.bankapp.ui.login
 
 import com.example.bank.bankapp.data.prefs.SharedPreferencesManager
+import java.util.regex.Pattern
 
 class LoginUsecase(private val repository: LoginRepository, private val prefs: SharedPreferencesManager) : LoginContract.Usecase {
 
     private val passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\S+$).{4,}\$"
 
     override fun validaUsername(username: String?): Boolean {
-        return false
+        return username == null || username.isBlank() || username.isEmpty()
     }
 
     override fun validaPassword(password: String?): Boolean {
-        return false
+        val pattern = Pattern.compile(passwordPattern)
+        val matcher = pattern.matcher(password)
+        return !matcher.matches()
     }
 
     override suspend fun login(username: String, password: String)
